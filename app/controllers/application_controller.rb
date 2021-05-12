62a81495559df8eeb710adab36ddee0552fadc4b
ryoujiohta <ohta.ryou1006@gmail.com>
2021-05-12 16:21:16 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  # ログイン画面に遷移するように実装
+  before_action :authenticate_user!
+  
+  # application_controller.rb ファイルにnameカラムの保存を可能とする記述
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  # データベースに保存する
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,keys: [:nickname, :last_name, :first_name,:last_name_kana, :first_name_kana, :birth_day])
+  end
 end
