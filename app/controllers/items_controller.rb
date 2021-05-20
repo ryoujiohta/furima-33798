741dd0f781d6a16f948e0777097c6014bda99e56
ryoujiohta <ohta.ryou1006@gmail.com>
2021-05-20 16:04:18 +0900

    ActiveHashの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,23 +2,34 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   
   def index
+    @items = Item.all.order(created_at: :desc)
   end
 
-  # def new
-  # end
+  def new
+    @item = Item.new
+  end
 
-  # def create
-  # end
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
-  # def show
-  # end
+  def show
+  end
 
-  # def edit
-  # end
-
-
+  def edit
+  end
 
 
+  def item_params
+    params.require(:item).permit(:name, :price, :description, :image, :category_id, :item_status_id, :prefecture_id,
+                                 :delivery_burden_id, :delivery_day_id).merge(user_id: current_user.id)
+  end
 
-  
+
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :area
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :burden
+  belongs_to :prefecture
+
 end
