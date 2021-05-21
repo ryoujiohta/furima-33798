5b7572c0b7cdf728ae2b9fa4fe40f162d3e0fa25
ryoujiohta <ohta.ryou1006@gmail.com>
2021-05-21 09:20:56 +0900

    バリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,24 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :area
   belongs_to :category
   belongs_to :condition
   belongs_to :burden
+  belongs_to :area
   belongs_to :prefecture
 
+  with_options presence: true do
+  #商品名、説明、画像、価格なしだと保存できないようにする
+    validates :name
+    validates :description
+    validates :price
+    validates :image
+
+    #activehashに対してカラでは保存できないようにする
+    validates :category_id
+    validates :item_status_id
+    validates :delivery_burden_id
+    validates :prefecture_id
+    validates :delivery_day_id
+    
 end
