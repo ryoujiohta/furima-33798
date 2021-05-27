7d5bae2ee93133f9cbc55536b98660b52dad33d8
ryoujiohta <ohta.ryou1006@gmail.com>
2021-05-28 08:18:04 +0900

    商品詳細ページの取得と表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -17,11 +18,12 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def show
-  # end
+  def show
+    @item = Item.find(params[:id])
+  end
 
-  # def edit
-  # end
+  def edit
+  end
 
   private
 
@@ -29,4 +31,6 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :price, :description, :image, :category_id, :condition_id, :prefecture_id,
                                  :burden_id, :area_id).merge(user_id: current_user.id)
   end
+
+
 end
